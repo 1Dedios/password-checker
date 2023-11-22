9daedc5b62ff2e4fdedbbd9c538f79b045d16890
Dedios <dediosprogramming@gmail.com>
2023-11-21 21:24:37 -0500

    user_input.py module created

---
+++ user_input.py
@@ -0,0 +1,11 @@
+
+
+# accept user's input
+password = input("Please enter the password you would like to validate. Only alphanumeric ( a-z, 0-9 ) characters are allowed and these special characters ( !#& ).")
+
+
+
+'''Linting through the user's input to make sure ONLY alphanumeric characters and a certain set of special characters are included.'''
+
+
+'''If it passes the linting we shall send it to be hashed'''
